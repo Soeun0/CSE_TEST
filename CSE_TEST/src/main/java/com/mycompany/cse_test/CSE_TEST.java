e8659ac08d3b5bc70512c3886d40e7fa37b6e591
Soeun0 <144868086+Soeun0@users.noreply.github.com>
2024-03-31 14:33:31 +0900

    [ADD]프로젝트 생성
    2023.03.31

---
+++ CSE_TEST/src/main/java/com/mycompany/cse_test/CSE_TEST.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cse_test;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CSE_TEST {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
